e3ca4def43ad0dc51b648f3b03ce6c88d13569a2
Matovu Ronald <maronwhite6@gmail.com>
2021-05-18 20:43:17 +0300

    Update FetchDataExample.js

--- src/components/useEffect/FetchDataExample.js
+++ src/components/useEffect/FetchDataExample.js
@@ -27,7 +27,7 @@ const FetchDataExample = () => {
               <img src={avatar_url} alt={login} />
               <div>
                 <h4>{login}</h4>
-                <a href={html_url} target="_blank">
+                <a href={html_url} target="_blank" rel="noreferrer">
                   Github Account
                 </a>
               </div>
